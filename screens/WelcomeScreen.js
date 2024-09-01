a96f8a64a8af2a3aeffd18dffd1c42bafc597688
Ralica Popova <ralicaterzieva90@gmail.com>
2024-09-01 19:43:40 +0300

    Get message from firebase

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,30 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from './../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://react-native-course-e29fb-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=' +
+          token
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
