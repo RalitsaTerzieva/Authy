4a88edb16f85d1d50614ffa2d42a18763d6659d2
Ralica Popova <ralicaterzieva90@gmail.com>
2024-09-01 14:21:34 +0300

    Add API KEY

---
+++ util/auth.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { API_KEY } from './config.js'; 
+
+export async function createUser(email, password) {
+  const response = await axios.post(
+    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }
+  );
+}
